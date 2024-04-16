b039e09a463f39974ae517ac4478e91e9da13971
Frank Trujillo <155175699+Frankielee2272@users.noreply.github.com>
2024-04-16 00:56:46 +0000

    13:10 Video Time

--- src/models.py
+++ src/models.py
@@ -4,6 +4,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80),nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -17,3 +18,35 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+    class Product(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(80),nullable=False)
+        password = db.Column(db.String(80), unique=False, nullable=False)
+        quantitiy = db.Column(db.Integer(), unique=False, nullable=False)
+        category_id = db.Column(db.Integer, db.ForeignKey('Category.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email
+        }
+
+    class Category(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(80),nullable=False)
+        password = db.Column(db.String(80), unique=False, nullable=False)
+        quantitiy = db.Column(db.Integer(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email
+        }
+
